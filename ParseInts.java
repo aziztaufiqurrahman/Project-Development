405feaec4b4839ef0bb26b6425ad2d95fdc54708
Muhammad Aziz Taufiqurrahman <79096529+aziztaufiqurrahman@users.noreply.github.com>
2021-06-28 14:24:01 +0700

    Create ParseInts.java

---
+++ ParseInts.java
@@ -0,0 +1,22 @@
+package Exception_ErrHandling;
+
+import java.util.Scanner;
+
+public class ParseInts {
+    public static void main(String[] args) {
+        int val, sum = 0;
+        Scanner scan = new Scanner(System.in);
+        String line;
+        System.out.println("Enter a line of text");
+        Scanner scanLine = new Scanner(scan.nextLine());
+
+        while (scanLine.hasNext()) {
+            try {	//Throws the exception
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            } catch (NumberFormatException e) {	//NumberFormatException
+            }
+        }
+        System.out.println("The sum of the integers on this line is " + sum);
+    }
+}
